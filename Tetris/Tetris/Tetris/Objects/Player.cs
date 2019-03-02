cad3ff25b11945f80db73e4c42fba41dd21302d0
NWTC-RodneyMushero <47837598+NWTC-RodneyMushero@users.noreply.github.com>
2019-03-02 14:18:23 -0800

    Update Player.cs

--- Tetris/Tetris/Tetris/Objects/Player.cs
+++ Tetris/Tetris/Tetris/Objects/Player.cs
@@ -1,5 +1,5 @@
-﻿//-----------------------------------------------------------------------
-// <copyright file="Player.cs" company="Group 2">
+//-----------------------------------------------------------------------
+// <copyright file="controlX.cs" company="Group 2">
 //     All Rights Reserved
 // </copyright>
 //-----------------------------------------------------------------------
@@ -55,7 +55,17 @@ namespace Tetris.Objects
         public int thirdY;
 
         /// <summary>
-        /// Chekcs if there is a player on the map.
+        /// 3X
+        /// </summary>
+        public int fourthX;
+
+        /// <summary>
+        /// 3Y
+        /// </summary>
+        public int fourthY;
+
+        /// <summary>
+        /// Chekcs if there is a controlX on the map.
         /// </summary>
         public bool isActive;
 
@@ -73,5 +83,112 @@ namespace Tetris.Objects
         /// Checks if there is an empty space to the right.
         /// </summary>
         public int typeRight;
+
+
+        public void square(int controlX, int controlY, int secondX, int secondY, int thirdX, int thirdY, int fourthX, int fourthY)
+        {
+            this.controlX = 4;
+            this.controlY = 0;
+        
+            this.secondX = 5;
+            this.secondY = 0;
+
+            this.thirdX = 4;
+            this.thirdY = 1;
+
+            this.fourthX = 5;
+            this.fourthY = 1;
+        }
+        public void iPiece(int controlX, int controlY, int secondX, int secondY, int thirdX, int thirdY, int fourthX, int fourthY)
+        {
+ // I piece
+        this.controlX = 3;
+       this.controlY = 0;
+
+      this.secondX = 4;
+       this.secondY = 0;
+
+      this.thirdX = 5;
+      this.thirdY = 0;
+            
+       this.fourthX = 6;
+       this.fourthY = 0;
+        }
+        public void jPiece(int controlX, int controlY, int secondX, int secondY, int thirdX, int thirdY, int fourthX, int fourthY)
+        {
+            // I piece
+            this.controlX = 3;
+            this.controlY = 0;
+
+            this.secondX = 4;
+            this.secondY = 0;
+
+            this.thirdX = 5;
+            this.thirdY = 0;
+
+            this.fourthX = 5;
+            this.fourthY = 1;
+        }
+        public void lPiece(int controlX, int controlY, int secondX, int secondY, int thirdX, int thirdY, int fourthX, int fourthY)
+        {
+            // I piece
+            this.controlX = 3;
+            this.controlY = 0;
+
+            this.secondX = 3;
+            this.secondY = 1;
+
+            this.thirdX = 4;
+            this.thirdY = 0;
+
+            this.fourthX = 5;
+            this.fourthY = 0;
+        }
+
+        public void sPiece(int controlX, int controlY, int secondX, int secondY, int thirdX, int thirdY, int fourthX, int fourthY)
+        {
+            // I piece
+            this.controlX = 3;
+            this.controlY = 1;
+
+            this.secondX = 4;
+            this.secondY = 0;
+
+            this.thirdX = 5;
+            this.thirdY = 0;
+
+            this.fourthX = 5;
+            this.fourthY = 1;
+        }
+        public void tPiece(int controlX, int controlY, int secondX, int secondY, int thirdX, int thirdY, int fourthX, int fourthY)
+        {
+            // I piece
+            this.controlX = 3;
+            this.controlY = 0;
+
+            this.secondX = 4;
+            this.secondY = 0;
+
+            this.thirdX = 5;
+            this.thirdY = 0;
+
+            this.fourthX = 4;
+            this.fourthY = 1;
+        }
+        public void zPiece(int controlX, int controlY, int secondX, int secondY, int thirdX, int thirdY, int fourthX, int fourthY)
+        {
+            // I piece
+            this.controlX = 3;
+            this.controlY = 0;
+
+            this.secondX = 4;
+            this.secondY = 0;
+
+            this.thirdX = 4;
+            this.thirdY = 1;
+
+            this.fourthX = 5;
+            this.fourthY = 1;
+        }
     }
 }
