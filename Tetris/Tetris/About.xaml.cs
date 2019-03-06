59aa1a8cb309cea10bdb108c94fe13e10d412552
NWTC-RodneyMushero <47837598+NWTC-RodneyMushero@users.noreply.github.com>
2019-03-06 17:26:55 -0600

    Add files via upload

---
+++ Tetris/Tetris/About.xaml.cs
@@ -0,0 +1,67 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="About.xaml.cs" company="Group 2">
+//     All Rights Reserved
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Tetris
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for About.xaml
+    /// </summary>
+    public partial class About : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="About"/> class.
+        /// </summary>
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Header of the rules.
+        /// </summary>
+        private string head;
+
+        /// <summary>
+        /// Rules fo teh game.
+        /// </summary>
+        private string desc;
+
+        /// <summary>
+        /// Back button click method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// To move the window around.
+        /// </summary>
+        private void About_Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+    }
+}
