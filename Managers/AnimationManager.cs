fa8ee71b2c6edbb2b5b50a7f8936ec299718364d
NWTC-RodneyMushero <47837598+NWTC-RodneyMushero@users.noreply.github.com>
2019-03-07 14:32:38 -0600

    Add files via upload

---
+++ Managers/AnimationManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace Tron
+{
+    public class AnimationManager
+    {
+        private Animation _animation;
+        private float _timer;
+    }
+}
